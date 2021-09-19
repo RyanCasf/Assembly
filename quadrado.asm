4c08b950059ba270ead3f6680ed18a3cded26c47
RyanCasf <ryaninternet62@gmail.com>
2021-09-19 10:04:59 -0300

    Operação de calcular o quadrado com entrada.

--- quadrado.asm
+++ quadrado.asm
@@ -1,16 +1,19 @@
 # Cálculo do quadrado do Número 5
 .data # Cadeia de pequenos de scaracteres (32 bits nos $a0)
-       escreva00: .asciiz"Quadrado = " #binário para ASCII
+	escrevaIntro: .asciiz "Digite o numero para obter o quadrado: "
 .text # Cadeia de grandes de caracteres
 main: # Início do código
 	
-	li $t0, 5
-	mul $t1, $t0, $t0
-	
 	li $v0, 4
-		la $a0, escreva00	# Atribuição .data -> registrador
-	syscall # interrupção
+	la $a0, escrevaIntro
+	syscall
+	
+	li $v0, 5
+	syscall
+
+	add $t0, $v0, 0
+	mul $t1, $t0, $t0
 
 	li $v0, 1
-		add $a0, $t1, 0
+	add $a0, $t1, 0
 	syscall
