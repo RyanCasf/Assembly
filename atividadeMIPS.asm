ba81fabbab67b5cf9e9c88b58f157e2437b83b32
RyanCasf <ryaninternet62@gmail.com>
2022-03-23 13:11:59 -0300

    Arquivos classificados e projeto final de asm

---
+++ atividadeMIPS.asm
@@ -0,0 +1,71 @@
+.data
+	escrevaIntro: .asciiz "AOC - Exercicio 59!" 
+	escrevaEntrada: .asciiz "\nDigite um numero de termos (n): "
+	escrevaSaida: .asciiz "\nRESULTADO DE (H) = "
+	escrevaSaidaMenor: .asciiz "* Numero digitado (-) negativo, nao pode gerar nenhum termo"
+	escrevaSaidaZero: .asciiz "* Numero digitado (0), valor de H = 0"
+.text
+main:
+	li $v0, 4
+	la $a0, escrevaIntro
+	syscall
+
+	li $v0, 4
+	la $a0, escrevaEntrada
+	syscall
+	
+	li $v0, 5
+	syscall
+	add $t0, $v0, 0
+	
+	blt $t0, 0 saidaMenor
+	beq $t0, 0 saidaZero
+	
+	li $t4, 1
+	j for
+
+saidaZero:
+	li $v0, 4
+	la $a0, escrevaSaidaZero
+	syscall
+	j fim
+	
+saidaMenor:
+	li $v0, 4
+	la $a0, escrevaSaidaMenor
+	syscall
+	j fim
+	
+decisao:
+	beq $t4, 1, dobro
+	j mono
+
+dobro:
+	li $t4, 2
+	j for
+	
+mono:
+	li $t4, 1
+	j for
+
+for:
+	add $t1, $t1, 1
+	
+	mul $t3, $t1, $t4
+	add $t2, $t2, $t3
+		
+	beq $t1, $t0, resultado
+	j decisao
+	
+resultado:
+	li $v0, 4
+	la $a0, escrevaSaida
+	syscall
+	
+	li $v0, 1
+	add $a0, $t2, 0
+	syscall
+	
+	j fim
+	
+fim:	
